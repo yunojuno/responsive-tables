71fc94fd024ffe08aa3746d17a3a41c976923b51
culshaw <culshaw@gmail.com>
2015-01-26 10:27:28 +0000

    Added hooks for ajax to latch onto

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   var switched = false;
   var updateTables = function() {
+      console.trace();
     if (($(window).width() < 767) && !switched ){
       switched = true;
       $("table.responsive").each(function(i, element) {
         splitTable($(element));
+          console.log('split');
+        $(element).trigger('responsiveTables.postDraw');
       });
       return true;
     }
@@ -12,37 +15,41 @@ $(document).ready(function() {
       switched = false;
       $("table.responsive").each(function(i, element) {
         unsplitTable($(element));
+          console.log('unsplit');
+          $(element).trigger('responsiveTables.postDraw');
       });
     }
-    
-    $(window).trigger('responsiveTables.postDraw');
   };
-   
+
   $(window).load(updateTables);
   $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
   $(window).on("resize", updateTables);
-   
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
+  $(window).on("ajax.postReply", function(){
+      switched=false;
+      updateTables();
+  });
+
+
+  function splitTable(original)
+  {
+    original.wrap("<div class='table-wrapper' />");
+
+    var copy = original.clone();
+    copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+    copy.removeClass("responsive");
+
+    original.closest(".table-wrapper").append(copy);
+    copy.wrap("<div class='pinned' />");
+    original.wrap("<div class='scrollable' />");
 
     setCellHeights(original, copy);
-	}
-	
-	function unsplitTable(original) {
+  }
+
+  function unsplitTable(original) {
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
     original.unwrap();
-	}
+  }
 
   function setCellHeights(original, copy) {
     var tr = original.find('tr'),
